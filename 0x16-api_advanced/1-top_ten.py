a16f101e576642151591c6877e79b1fe5992601d
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-08-14 01:24:19 +0300

    Task 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,7 +3,7 @@
 
 
 def top_ten(subreddit):
-    """a function that queries the Reddit API and prints the titles of the 
+    """a function that queries the Reddit API and prints the titles of the
     first 10 hot posts listed for a given subreddit."""
     import requests
 
